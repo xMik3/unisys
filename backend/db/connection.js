025307ba39de572a3bf183cd27eea1017a995747
Michael Ieronymidis <ieronymidismichael@gmail.com>
2026-07-25 01:25:40 +0300

    refresh db init schema

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -6,7 +6,8 @@ let db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    flags:['FOUND_ROWS']
 });
 
 export default db;
